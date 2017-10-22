a71929f779e327549cfaf3a73e7deec45e75daa6
Stasik <scherkasov8@gmail.com>
2017-10-22 21:38:23 +0300

    added LoginViewController

---
+++ CoinsHunter/CoinsHunter/LoginPageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginPageViewController.swift
+//  CoinsHunter
+//
+//  Created by Stanislav Cherkasov on 22.10.17.
+//  Copyright © 2017 Stanislav Cherkasov. All rights reserved.
+//
+
+import UIKit
+
+class LoginPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
