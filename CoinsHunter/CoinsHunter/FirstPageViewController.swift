3e9807ef3586915d63c5b6b7232dc74f320cd1b2
Stasik <scherkasov8@gmail.com>
2017-10-22 21:07:58 +0300

    added NavigationController and his translucent

--- CoinsHunter/CoinsHunter/FirstPageViewController.swift
+++ CoinsHunter/CoinsHunter/FirstPageViewController.swift
@@ -8,28 +8,29 @@
 
 import UIKit
 
+extension UILabel {
+    func blink() {
+        self.alpha = 0.0;
+        UIView.animate(withDuration: 0.9, //Time of duration
+            delay: 0.0,
+            options: [.autoreverse, .repeat],
+            animations: { [weak self] in self?.alpha = 1.0 },
+            completion: { [weak self] _ in self?.alpha = 0.0 })
+    }
+}
+
 class FirstPageViewController: UIViewController {
+    
+    
+    @IBAction func oneEuroStartButton(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var blinkingLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.blinkingLabel.blink()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- CoinsHunter/CoinsHunter/SecondPageViewController.swift
+++ CoinsHunter/CoinsHunter/SecondPageViewController.swift
@@ -8,28 +8,71 @@
 
 import UIKit
 
-class SecondPageViewController: UIViewController {
+class SecondPageViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var slideScrollView: UIScrollView!
 
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        slideScrollView.delegate = self
+        
+        let slides: [Slide] = createSlides()
+        setupSlideScrollView(slides: slides)
+        pageControl.numberOfPages = slides.count
+        pageControl.currentPage = 0
+        view.bringSubview(toFront: pageControl)
+        
+        tanslucentNC()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func createSlides() -> [Slide] {
+        
+        let firstSlide: Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        firstSlide.image.image = #imageLiteral(resourceName: "firstImage")
+        
+        let secondSlide: Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        secondSlide.image.image = #imageLiteral(resourceName: "secondImage")
+        
+        let thirdSlide: Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        thirdSlide.image.image = #imageLiteral(resourceName: "thirdImage")
+        
+        return [firstSlide, secondSlide, thirdSlide]
     }
-    */
-
+    
+    func setupSlideScrollView(slides: [Slide]) {
+        
+        slideScrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        
+        slideScrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+        
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+            
+            slideScrollView.addSubview(slides[i])
+            
+            slideScrollView.isPagingEnabled = true
+        }
+        
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
+        pageControl.currentPage = Int(pageIndex)
+    }
+    
+    func tanslucentNC() {
+        
+        // tanslucent navigationController
+        
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = UIColor.clear
+    }
+    
+    
 }
